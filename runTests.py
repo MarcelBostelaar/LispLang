d5eba8d51b093a569e10543105c4eb92627296c3
Marcel Bostelaar <marcel.bostelaar@gmail.com>
2024-10-09 21:50:26 +0200

    Moved tests up a level.

--- runTests.py
+++ runTests.py
@@ -1,5 +1,6 @@
 from functools import partial
-#
+import os
+print("Current Working Directory:", os.getcwd())
 from LispLangInterpreter.Config import errorMessages
 from LispLangInterpreter.ImportHandlerSystem.placeholderConfigs import exampleConfig
 from LispLangInterpreter.Parser.ParserCombinator import ParseError
@@ -8,6 +9,7 @@ from Tests.ParseTests.TestRunner import parseTest, parseErrorTest
 #
 from Tests.ParseTests import test1Expected, EOFCommentExpected
 from Tests.uncompiledRuntimeTest.TestRunner import uncompiledRuntimeTest
+import os
 
 # from Tests.uncompiledRuntimeTest.TestRunner import uncompiledRuntimeTest
 #
@@ -18,12 +20,12 @@ from Tests.uncompiledRuntimeTest.TestRunner import uncompiledRuntimeTest
 
 ########################################################
 
-parseTest("ParseTests/test1.lisp", test1Expected.expected, "Parse Tests 1")
-parseTest("ParseTests/EOFComment.lisp", EOFCommentExpected.expected, "EOF comment")
-parseErrorTest("ParseTests/unclosedStringTest.lisp", ParseError(1, errorMessages.unclosedString), "Unclosed string test")
-parseErrorTest("ParseTests/unmatchedBracketTest.lisp", ParseError(1, errorMessages.unclosedBracket), "Unmatched Bracket Test", "ParseTests/unmatchedBracketTestCorrect.lisp")
+parseTest("Tests/ParseTests/test1.lisp", test1Expected.expected, "Parse Tests 1")
+parseTest("Tests/ParseTests/EOFComment.lisp", EOFCommentExpected.expected, "EOF comment")
+parseErrorTest("Tests/ParseTests/unclosedStringTest.lisp", ParseError(1, errorMessages.unclosedString), "Unclosed string test")
+parseErrorTest("Tests/ParseTests/unmatchedBracketTest.lisp", ParseError(1, errorMessages.unclosedBracket), "Unmatched Bracket Test", "Tests/ParseTests/unmatchedBracketTestCorrect.lisp")
 
-uncompiledRuntimeTest(False, exampleConfig, "uncompiledRuntimeTest/sumtest1real.lisp", "uncompiledRuntimeTest/sumtest1expected.lisp", "Sum test 1")
+uncompiledRuntimeTest(False, exampleConfig, "Tests/uncompiledRuntimeTest/sumtest1real.lisp", "Tests/uncompiledRuntimeTest/sumtest1expected.lisp", "Sum test 1")
 # uncompiledRuntimeTest("uncompiledRuntimeTest/sumtest2real.lisp", "uncompiledRuntimeTest/sumtest2expected.lisp", "Sum test 2")
 # uncompiledRuntimeTest("uncompiledRuntimeTest/listEvaluationReal.lisp", "uncompiledRuntimeTest/listEvaluationExpected.lisp", "List evaluation test")
 # uncompiledRuntimeTest("uncompiledRuntimeTest/handleTest1Real.lisp", "uncompiledRuntimeTest/handleTest1Expected.lisp", "Handle test")
